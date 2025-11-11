da828328957bc58030dfca5bc310c28b886eaf94
ABHIGH2025 <amit.gupta2023@vitstudent.ac.in>
2025-11-11 14:59:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -92,4 +92,5 @@ def ping():
 # --- App Entry Point ---
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port)
+
